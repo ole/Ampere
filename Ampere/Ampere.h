259c0dcd3415bf2d0f0e324fe7b9497aa01babc9
Ole Begemann <ole@oleb.net>
2016-07-26 20:34:10 +0200

    Create iOS framework project in Xcode

---
+++ Ampere/Ampere.h
@@ -0,0 +1,19 @@
+//
+//  Ampere.h
+//  Ampere
+//
+//  Created by Ole Begemann on 26/07/16.
+//  Copyright © 2016 Ole Begemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ampere.
+FOUNDATION_EXPORT double AmpereVersionNumber;
+
+//! Project version string for Ampere.
+FOUNDATION_EXPORT const unsigned char AmpereVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ampere/PublicHeader.h>
+
+
